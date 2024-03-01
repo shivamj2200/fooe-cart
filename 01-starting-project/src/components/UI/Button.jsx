c109f21f8364263c5744f16653369d17f047373e
shivamjaiswal2200@gmail.com <shivamjaiswal2200@gmail.com>
2024-03-01 06:26:47 +0530

    Intial

---
+++ 01-starting-project/src/components/UI/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Button({children,textOnly,className,...props}) {
+    
+    let  cssClasses =  textOnly ? 'text-button' : 'button';
+      cssClasses += ' '+cssClasses
+    return (
+    <button className={cssClasses}{...props}>{children}</button>
+  )
+}
